8c78d3ac0c67c99ee052ca528e2863bc2de4c279
Ahmad Musaffa <musaffa_csemm@yahoo.com>
2025-06-27 03:34:38 +0600

    Add ruby_lsp

--- vim/lua/plugins/lsp.lua
+++ vim/lua/plugins/lsp.lua
@@ -1,6 +1,13 @@
-require("mason-lspconfig").setup({
+require('mason-lspconfig').setup({
   ensure_installed = {
     'ts_ls',
     'ember'
   }
 })
+
+-- ruby
+local ruby = tonumber(vim.fn.system("ruby -v"):match("ruby (%d+)"))
+
+if ruby and ruby >= 3 then
+  vim.lsp.enable('ruby_lsp')
+end
