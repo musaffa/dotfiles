2bd8de0e2f0769f7359e4f692e089c04f6f9b6d3
Ahmad Musaffa <musaffa_csemm@yahoo.com>
2025-07-01 16:51:48 +0600

    Update neovim file finder keymap

--- vim/lua/plugins/finder.lua
+++ vim/lua/plugins/finder.lua
@@ -15,13 +15,13 @@ telescope.load_extension 'ui-select'
 local builtin = require 'telescope.builtin'
 
 vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', '<leader>ss', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+vim.keymap.set('n', '<leader>ss', builtin.live_grep, { desc = '[S]earch by Grep' })
 vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = '[ ] Find existing buffers' })
 vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
 vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
 
 -- Shortcut for searching your Neovim configuration files
-vim.keymap.set('n', '<leader>fn', function()
+vim.keymap.set('n', '<leader>sn', function()
   builtin.find_files { cwd = vim.fn.stdpath 'config' }
 end, { desc = '[S]earch [N]eovim files' })
