a1294c86d0f2349662acfc2c671f2790e091b2d1
Ahmad Musaffa <musaffa_csemm@yahoo.com>
2025-07-07 01:17:24 +0600

    Disable language providers

--- vim/lua/config/options.lua
+++ vim/lua/config/options.lua
@@ -39,3 +39,8 @@ vim.filetype.add {
     hbs = 'handlebars',
   },
 }
+
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
