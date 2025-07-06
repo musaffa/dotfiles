e525164fbae873393711a6c855bbab6520a7a429
Ahmad Musaffa <musaffa_csemm@yahoo.com>
2025-07-07 01:13:38 +0600

    Add missing build command to LuaSnip

--- vim/lua/config/lazy.lua
+++ vim/lua/config/lazy.lua
@@ -87,6 +87,7 @@ require('lazy').setup {
     {
       'L3MON4D3/LuaSnip',
       version = 'v2.*',
+      build = 'make install_jsregexp',
       dependencies = {
         { 'rafamadriz/friendly-snippets' },
       },
